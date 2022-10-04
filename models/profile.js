61034a100183e27ffd333fbb446c562f7cf72c94
Leon Chu <Ljchu87@gmail.com>
2022-10-03 22:38:49 -0500

    Create coffeeSchema

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,25 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+  const coffeeSchema = new Schema ({
+    name: {
+      type: String,
+      enum: ['Coffee', 'Cold Brew', 'Latte', 'Americano', 'Macchiato']
+    },
+    temperature: {
+      type: String,
+      enum: ['Hot', 'Iced']
+    },
+    milk: {
+      type: String,
+      enum: ['Whole', '2%', 'Almond', 'Oat', 'Soy']
+    },
+  })
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  coffee: [coffeeSchema],
   owner: {type: Schema.Types.ObjectId, ref: "Profile"}
 }, {
   timestamps: true
