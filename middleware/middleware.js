3f6b74288e1617e61df5d04d8600f19af9ade9f6
Leon Chu <Ljchu87@gmail.com>
2022-10-06 10:02:38 -0500

    Update isLoggedIn function to reroute to homepage if user is not logged in

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -6,7 +6,8 @@ function passDataToView(req, res, next) {
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
-  res.redirect('/auth/google')
+  res.redirect('/')
+  // res.redirect('/auth/google')
 }
 
 export {
